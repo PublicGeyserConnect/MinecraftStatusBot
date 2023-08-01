4a130e4ccc4c97bea4358a5451108618289e9e8a
jensc <jenscollaert@hotmail.com>
2023-08-01 15:02:23 +0200

    dead import.

--- src/main/java/com/github/jensco/commands/HelpCommand.java
+++ src/main/java/com/github/jensco/commands/HelpCommand.java
@@ -2,11 +2,11 @@ package com.github.jensco.commands;
 
 import com.github.jensco.Bot;
 import com.github.jensco.util.BotColors;
-import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import org.jetbrains.annotations.NotNull;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
@@ -20,7 +20,7 @@ public class HelpCommand extends SlashCommand {
     }
 
     @Override
-    protected void execute(SlashCommandEvent event) {
+    protected void execute(@NotNull SlashCommandEvent event) {
         try {
             event.replyEmbeds(Collections.singleton(handle())).queue();
         } catch (InvocationTargetException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
@@ -28,6 +28,7 @@ public class HelpCommand extends SlashCommand {
         }
     }
 
+    @NotNull
     private MessageEmbed handle() throws InvocationTargetException, IllegalAccessException, InstantiationException, NoSuchMethodException {
         EmbedBuilder helpEmbed = new EmbedBuilder()
                 .setColor(BotColors.SUCCESS.getColor())
